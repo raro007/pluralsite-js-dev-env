0cad26ae8aef2bbfc493e69ef8127a32d53d060e
raro007 <31142072+raro007@users.noreply.github.com>
2020-07-05 16:45:55 +0100

    index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -17,7 +17,7 @@ describe('index.html',()=>
         jsdom.env(index,function(err,window){
 
             const h1=window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Hello");
+            expect(h1.innerHTML).to.equal("Hello!");
             done();
             window.close();
         })
